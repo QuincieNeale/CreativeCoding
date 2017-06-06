794093d0f308e44197d2da242a7e4b305f0f8c04
Quincie Neale <catq@sbcglobal.net>
2017-06-06 15:25:47 -0600

    Commit Working Exercise2

--- Exercise2/src/ofApp.cpp
+++ Exercise2/src/ofApp.cpp
@@ -2,107 +2,69 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+    
+    //title at top of screen
+    ofSetWindowTitle("Exercise_2");
 
-//title at top of screen
-    ofSetWindowTitle("SoLost");
-    
-//resolution for your shapes
-    ofSetCircleResolution(50);
-    
-    ofBackground(ofColor::white);
-    
-    
-    pos.x = ofRandom(ofGetWidth()/2);
-    pos.y = ofRandom(ofGetHeight()/2);
-    
-    vel.x = ofRandom(-4,4);
-    vel.y = ofRandom(-4,4);
-    
-    acc = vel.getLimited(1); //get same direction as vel, but limited to max length of 1
-    
-    ofLog() << "pos: " << pos << "vel: " << vel;
-    ofLog() << "speed: " << vel.length() << "heading: " << pos.angle(vel);
-    
-    
+    //background color
+    ofBackground(0, 0, 0);
     
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-    if(boost) {
-        vel = vel +acc;
-    }
-    
-    pos = pos + vel; //update position using velocity
-    
-    if(pos.x < 0) {
-        pos.x = 0;
-        vel= -vel.x;
-    }
-    if(pos.x > ofGetWidth()) {
-        pos.x = ofGetWidth();
-        vel = -vel.x;
-    }
-    if(pos.y < 0) {
-        pos.y = 0;
-        vel= -vel.x;
-    }
-    if(pos.y > ofGetWidth()) {
-        pos.y = ofGetWidth();
-        vel = -vel.x;
-    }
+
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-   
-    //draw circle
-    ofSetColor(ofColor::darkRed);
-    ofSetLineWidth(1);
-    ofDrawCircle(pos, 20);
-    ofDrawLine(0, 0, pos.x, pos.y);
     
-    //draw the direction and speed
-    ofSetColor(ofColor::green);
-    ofSetLineWidth(2);
-    ofDrawLine(pos.x, pos.y, pos.x+vel.x, pos.y+vel.y);
+    //translate(width/2, height/2);
+    ofTranslate(mouseX, mouseY);
+    
+    //rotate around the origin
+    //ofRotate(ellipse);
+    
+    ofColor c = moodmachine;
+    ofEllipse(100, 0, 50, 50);
+    
     
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
 
-    if(key == ' ') {
-        boost = true;
-    }
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
 
-    if(key == ' ') {
-        boost = false;
-    }
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y){
+void ofApp::mouseMoved(int x, int y ){
 
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
 
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
 
+    ofDrawCircle( PosX, PosY, 115);
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-
+    
+    ofCircle(160, 270, 115);
+    
 }
 
 //--------------------------------------------------------------
